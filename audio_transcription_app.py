03fc5f1a206e882b2347008e9e8c7ca619bcf35a
marvin evins <marvinevins@yahoo.com>
2023-05-29 14:53:45 -0500

    first commit

---
+++ audio_transcription_app.py
@@ -0,0 +1,21 @@
+from turtle import title
+import gradio as gr
+# import speech_recognition as sr
+import whisper
+
+def transcribe_speech(audio_file):
+    model = whisper.load_model("base")
+    result = model.transcribe( audio_file)
+
+    return result["text"]
+
+def main():
+    audio_input = gr.inputs.Audio(source="upload", type="filepath")
+    output_text = gr.outputs.Textbox()
+
+    iface = gr.Interface(fn=transcribe_speech, inputs=audio_input, outputs=output_text,title="Audio transcription", description="Transcribe audio files to text")
+
+    iface.launch()
+
+if __name__ == "__main__":
+    main()
